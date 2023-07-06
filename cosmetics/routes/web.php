7c8d9276848da76d49bbcdc0b2307ca059615bdb
edilsonsj <edilsonsjf@gmail.com>
2023-07-06 08:58:15 -0300

    added function to show products at index page

--- cosmetics/routes/web.php
+++ cosmetics/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\ProductController;
+
+Route::get('/', [ProductController::class, 'index']);
+
+
+Route::get('/products/show', [ProductController::class, 'show']);
